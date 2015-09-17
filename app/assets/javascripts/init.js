0d2ff01f7e6b4881f21387089de2f95156406868
SivoP <siv.padhy@gmail.com>
2015-09-16 17:44:12 -0700

    basic parallax function working, needed to use image_tag helper instead of directly linking

---
+++ app/assets/javascripts/init.js
@@ -0,0 +1,10 @@
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+    $('.parallax').parallax();
+    $('body').on('click', function(){
+      alert('hello!')
+    })
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
