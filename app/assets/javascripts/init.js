4fb9b5ee467535605cc67b34a62b8273237e3eb1
SivoP <siv.padhy@gmail.com>
2015-09-16 18:04:23 -0700

    added materialize icons via a CDN temporarily

--- app/assets/javascripts/init.js
+++ app/assets/javascripts/init.js
@@ -3,8 +3,6 @@
 
     $('.button-collapse').sideNav();
     $('.parallax').parallax();
-    $('body').on('click', function(){
-      alert('hello!')
-    })
+
   }); // end of document ready
 })(jQuery); // end of jQuery name space
