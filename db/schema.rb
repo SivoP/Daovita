f25f7c7a7f22c7c59663ac4842ab71763f5f9f1d
SivoP <siv.padhy@gmail.com>
2015-09-20 16:20:00 -0700

    basic teammate and contact seed data created and seeded into db

--- db/schema.rb
+++ db/schema.rb
@@ -30,6 +30,11 @@ ActiveRecord::Schema.define(version: 20150920182021) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "projects", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "resources", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+  # CURRENTLY SEEDING TEAMMATES INTO DB WITH NO PROFILE PICTURE BEING USED
+    teammates = Teammate.create([
+        {
+          firstname: "Mark",
+          lastname: "Wijiya",
+          title:    "Co-founder",
+          description: "Mark is an expert in education and 3D printing",
+          email: "mark.anthony.wijaya@gmail.com",
+          phone: "604-379-5151"
+        },
+        {
+          firstname: "Siv",
+          lastname: "Padhy",
+          title:    "Co-founder",
+          description: "Siv is a passionate about technology and education",
+          email: "siv.padhy@gmail.com",
+          phone:"778-995-0965"
+        }
+      ])
+
+      # VALIDATIONS STILL NEED TO BE WRITTEN, BUT ALSO WE DONT HAVE LOCATION AND COMPANY SPECIFIC EMAIL
+    contact = Contact.create({
+      location: "Vancouver, BC",
+      phone: "778-995-0965",
+      email: "ideasfu@gmail.com"
+      })
+
+
+     
