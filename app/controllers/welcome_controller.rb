2636ed0dc7bb7b0d0c1b206f04474476df288d3b
SivoP <siv.padhy@gmail.com>
2015-09-27 17:41:19 -0700

    user model created and migrated

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,6 @@
 class WelcomeController < ApplicationController
   def index
-    @partners = Partner.all
-    @teammates = Teammate.all
+
     render :index
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,46 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150920182021) do
+ActiveRecord::Schema.define(version: 20150928003756) do
 
-  create_table "contacts", force: :cascade do |t|
-    t.string   "location"
-    t.string   "phone"
-    t.string   "email"
-    t.text     "description"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-  end
-
-  create_table "partners", force: :cascade do |t|
-    t.string   "name"
-    t.string   "logo"
-    t.string   "page_url"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "projects", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "resources", force: :cascade do |t|
+  create_table "users", force: :cascade do |t|
+    t.string   "firstname"
+    t.string   "lastname"
+    t.integer  "age"
     t.string   "title"
-    t.text     "description"
-    t.string   "url"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-  end
-
-  create_table "teammates", force: :cascade do |t|
-    t.string "firstname"
-    t.string "lastname"
-    t.string "title"
-    t.text   "description"
-    t.string "image_url"
-    t.string "email"
-    t.string "phone"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
